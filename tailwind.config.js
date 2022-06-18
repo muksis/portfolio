7aba3ce0d15c1551d9463b7b8798884057aab6f7
muksis <60219890+muksis@users.noreply.github.com>
2022-06-17 20:11:34 -0400

    Add font changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        'space-mono': ['"Space Mono"', "Trispace", '"Courier New"'],
+        'space-mono': ['"Space Mono"', '"Trispace"', '"Courier New"'],
       },
     },
   },
