a8257f89dd09d00f9627d65aa23e94550d6a0a60
muksis <60219890+muksis@users.noreply.github.com>
2022-06-25 15:48:07 -0400

    Add required to the form fields

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,9 +5,9 @@ const Contact = () => {
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-black">Contact</p>
         </div>
-        <input className="border-2 border-black p-2" type="text" placeholder="Name" name="name" />
-        <input className="border-2 border-black my-4 p-2" type="email" placeholder="Email" name="email" />
-        <textarea className="border-2 border-black p-2" placeholder="Message" name="message" rows="10"></textarea>
+        <input className="border-2 border-black p-2" type="text" placeholder="Name" name="name" required />
+        <input className="border-2 border-black my-4 p-2" type="email" placeholder="Email" name="email" required />
+        <textarea className="border-2 border-black p-2" placeholder="Message" name="message" rows="10" required></textarea>
         <button className="border-2 border-black hover:bg-black hover:text-white px-4 py-3 my-8 mx-auto flex items-center">Submit</button>
       </form>
     </div>
