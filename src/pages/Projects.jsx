d6442af3c3401e9da06a4cb573f02ba7de0c5355
muksis <60219890+muksis@users.noreply.github.com>
2022-07-27 12:04:22 -0400

    Add layout fixes

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,27 +1,49 @@
-import ProjectCard from "../components/ProjectCard";
-import exterior_1 from "../assets/exterior_1.jpeg";
-import exterior_2 from "../assets/exterior_2.jpeg";
-import exterior_3 from "../assets/exterior_3.jpeg";
+import ProjectCard from '../components/ProjectCard';
+import exterior_1 from '../assets/exterior_1.jpeg';
+import exterior_2 from '../assets/exterior_2.jpeg';
+import exterior_3 from '../assets/exterior_3.jpeg';
 
 const Projects = () => {
   return (
-    <div className="w-full md:h-screen">
+    <div className='w-full h-screen'>
       {/* Container */}
-      <div className="max-w-[1000px] mx-auto p-4 flex-col justify-center w-full h-full">
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-black">Projects</p>
-          <p className="py-6">// These are some of my recent projects</p>
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <p className='text-4xl font-bold inline border-b-4 border-black'>
+            Projects
+          </p>
+          <p className='py-6'>// These are some of my recent projects</p>
         </div>
         {/* Projects container*/}
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
-          <ProjectCard url={exterior_1} name="Project 1" demoRef="/" codeRef="/" />
-          <ProjectCard url={exterior_2} name="Project 2" demoRef="/" codeRef="/" />
-          <ProjectCard url={exterior_3} name="Project 3" demoRef="/" codeRef="/" />
+        <div className='grid sm:grid-cols-2 md:grid-cols-2 gap-4'>
+          <ProjectCard
+            url={exterior_1}
+            name='Project 1'
+            demoRef='/'
+            codeRef='/'
+          />
+          <ProjectCard
+            url={exterior_2}
+            name='Project 2'
+            demoRef='/'
+            codeRef='/'
+          />
+          <ProjectCard
+            url={exterior_3}
+            name='Project 3'
+            demoRef='/'
+            codeRef='/'
+          />
+          <ProjectCard
+            url={exterior_3}
+            name='Project 4'
+            demoRef='/'
+            codeRef='/'
+          />
         </div>
       </div>
     </div>
   );
-}
-  
+};
+
 export default Projects;
-  
--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,38 +1,94 @@
-import SkillCard from "../components/SkillCard";
-import { SiHtml5, SiCss3, SiJavascript, SiReact, SiGit, SiGithub, SiRedux, SiNextdotjs, SiExpress, SiNodedotjs, SiMongodb, SiFirebase, SiBootstrap, SiTailwindcss, SiDocker, SiPostman, SiJest } from "react-icons/si";
+import SkillCard from '../components/SkillCard';
+import {
+  SiHtml5,
+  SiCss3,
+  SiJavascript,
+  SiReact,
+  SiGit,
+  SiGithub,
+  SiRedux,
+  SiNextdotjs,
+  SiExpress,
+  SiNodedotjs,
+  SiMongodb,
+  SiFirebase,
+  SiBootstrap,
+  SiTailwindcss,
+  SiDocker,
+  SiPostman,
+  SiJest,
+} from 'react-icons/si';
 
 const Skills = () => {
-    return (
-      <div className="w-full h-screen">
-        {/* Container */}
-        <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-          <div>
-            <p className="text-4xl font-bold inline border-b-4 border-black">Skills</p>
-            <p className="py-4">// These are the technologies I've worked with</p>
-          </div>
+  return (
+    <div className='w-full h-screen'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div>
+          <p className='text-4xl font-bold inline border-b-4 border-black'>
+            Skills
+          </p>
+          <p className='py-4'>// These are the technologies I've worked with</p>
+        </div>
 
-          <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
-            <SkillCard icon={<SiHtml5 className="w-20 mx-auto" />} name="HTML" />
-            <SkillCard icon={<SiCss3 className="w-20 mx-auto" />} name="CSS" />
-            <SkillCard icon={<SiJavascript className="w-20 mx-auto" />} name="JS" />
-            <SkillCard icon={<SiReact className="w-20 mx-auto" />} name="ReactJS" />
-            <SkillCard icon={<SiGit className="w-20 mx-auto" />} name="Git" />
-            <SkillCard icon={<SiRedux className="w-20 mx-auto" />} name="Redux" />
-            <SkillCard icon={<SiNextdotjs className="w-20 mx-auto" />} name="Next.js" />
-            <SkillCard icon={<SiExpress className="w-20 mx-auto" />} name="Express" />
-            <SkillCard icon={<SiNodedotjs className="w-20 mx-auto" />} name="Node.js" />
-            <SkillCard icon={<SiMongodb className="w-20 mx-auto" />} name="MongoDB" />
-            <SkillCard icon={<SiFirebase className="w-20 mx-auto" />} name="Firebase" />
-            <SkillCard icon={<SiBootstrap className="w-20 mx-auto" />} name="Bootstrap" />
-            <SkillCard icon={<SiTailwindcss className="w-20 mx-auto" />} name="Tailwind CSS" />
-            <SkillCard icon={<SiDocker className="w-20 mx-auto" />} name="Docker" />
-            <SkillCard icon={<SiPostman className="w-20 mx-auto" />} name="Postman" />
-            <SkillCard icon={<SiJest className="w-20 mx-auto" />} name="Jest" />
-          </div>
+        <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+          <SkillCard icon={<SiHtml5 className='w-20 mx-auto' />} name='HTML' />
+          <SkillCard icon={<SiCss3 className='w-20 mx-auto' />} name='CSS' />
+          <SkillCard
+            icon={<SiJavascript className='w-20 mx-auto' />}
+            name='JS'
+          />
+          <SkillCard
+            icon={<SiReact className='w-20 mx-auto' />}
+            name='ReactJS'
+          />
+          <SkillCard
+            icon={<SiGithub className='w-20 mx-auto' />}
+            name='GitHub'
+          />
+          <SkillCard icon={<SiGit className='w-20 mx-auto' />} name='Git' />
+          <SkillCard icon={<SiRedux className='w-20 mx-auto' />} name='Redux' />
+          <SkillCard
+            icon={<SiNextdotjs className='w-20 mx-auto' />}
+            name='Next.js'
+          />
+          <SkillCard
+            icon={<SiExpress className='w-20 mx-auto' />}
+            name='Express'
+          />
+          <SkillCard
+            icon={<SiNodedotjs className='w-20 mx-auto' />}
+            name='Node.js'
+          />
+          <SkillCard
+            icon={<SiMongodb className='w-20 mx-auto' />}
+            name='MongoDB'
+          />
+          <SkillCard
+            icon={<SiFirebase className='w-20 mx-auto' />}
+            name='Firebase'
+          />
+          <SkillCard
+            icon={<SiBootstrap className='w-20 mx-auto' />}
+            name='Bootstrap'
+          />
+          <SkillCard
+            icon={<SiTailwindcss className='w-20 mx-auto' />}
+            name='Tailwind CSS'
+          />
+          <SkillCard
+            icon={<SiDocker className='w-20 mx-auto' />}
+            name='Docker'
+          />
+          <SkillCard
+            icon={<SiPostman className='w-20 mx-auto' />}
+            name='Postman'
+          />
+          <SkillCard icon={<SiJest className='w-20 mx-auto' />} name='Jest' />
         </div>
       </div>
-    );
-  }
-      
-  export default Skills;
-      
+    </div>
+  );
+};
+
+export default Skills;
